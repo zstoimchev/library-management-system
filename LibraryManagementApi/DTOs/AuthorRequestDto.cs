e2e43529042d8c4260c24b946dbfd295551a092a
Zhivko <zivkostoimcev1551@outlook.com>
2025-04-13 13:58:22 +0200

    [ADD] added TODOs yet to be implemented

--- LibraryManagementApi/DTOs/AuthorRequestDto.cs
+++ LibraryManagementApi/DTOs/AuthorRequestDto.cs
@@ -2,6 +2,7 @@ namespace LibraryManagement.Dtos;
 
 public class AuthorRequestDto
 {
+    // TODO: Add validation attributes
     public string Name { get; set; } = string.Empty;
     public DateTime DateOfBirth { get; set; }
 }
--- LibraryManagementApi/DTOs/BookRequestDto.cs
+++ LibraryManagementApi/DTOs/BookRequestDto.cs
@@ -2,6 +2,7 @@ namespace LibraryManagement.Dtos;
 
 public class BookRequestDto
 {
+    // TODO: Add validation attributes
     public string Title { get; set; } = string.Empty;
     public int PublicationYear { get; set; }
     public int AuthorId { get; set; }
