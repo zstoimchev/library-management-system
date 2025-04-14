ee59882f7df1ff57f3c1a92fc36ce2f164e2559d
Zhivko <zivkostoimcev1551@outlook.com>
2025-04-14 16:28:49 +0200

    [UPDATE] fetching books and authors globally instead in BookCollection

--- library-client/src/App.js
+++ library-client/src/App.js
@@ -3,8 +3,20 @@ import Layout from "./Components/Layout.jsx";
 import BookCollection from "./Pages/BookCollection.jsx";
 import AddEditBook from "./Pages/AddEditBook.jsx";
 import AddAuthor from "./Pages/AddAuthor.jsx";
+import {useEffect} from "react";
+import {fetchBooks} from "./Store/bookSlice";
+import {fetchAuthors} from "./Store/authorSlice";
+import {useDispatch} from "react-redux";
 
 function App() {
+    const dispatch = useDispatch();
+    const pageNumber = 1;
+    const pageSize = 10;
+
+    useEffect(() => {
+        dispatch(fetchBooks({ pageNumber, pageSize }));
+        dispatch(fetchAuthors());
+    }, [dispatch]);
     return (<Router>
         <Layout>
             <Routes>
