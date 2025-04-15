b21d760926fde1aee37c35f79d11d56557809826
Zhivko <zivkostoimcev1551@outlook.com>
2025-04-15 14:43:22 +0200

    [FIX] adjusted validation for date of birth

--- LibraryManagementApi/LibraryManagementApi/Validators/DoBValidationAttribute.cs
+++ LibraryManagementApi/LibraryManagementApi/Validators/DoBValidationAttribute.cs
@@ -6,12 +6,12 @@ public class DoBValidationAttribute : ValidationAttribute
 {
     protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
     {
-        if (value is not int year)
+        if (value is not DateTime dateOfBirth)
         {
-            return new ValidationResult("Date of Birth Year is required.");
+            return new ValidationResult("Invalid date format. Date of Birth must be a valid DateTime.");
         }
 
-        return year >= DateTime.UtcNow.Year
+        return dateOfBirth.Year >= DateTime.UtcNow.Year
             ? new ValidationResult("Date of Birth Year must be before the current year.")
             : ValidationResult.Success;
     }
